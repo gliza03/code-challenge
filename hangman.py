fa836a61a195a3968dbd5bde0152f4a9d076c25c
gliza03 <99229900+gliza03@users.noreply.github.com>
2023-11-16 20:33:41 -0700

    Debug 1

--- hangman.py
+++ hangman.py
@@ -2,7 +2,7 @@ import random
 
 # Greeting
 name = input("What is your name?")
-print ("Hello %s, Welcome to Hangman!", name)
+print ("Hello &s, Welcome to Hangman!", name)
 
 # Define list of words
 words = ["charm", "pretty", "obstacle", "difficulty", "speaker", "Peru", "sun", "vitamins", "fan", "glasses"]
