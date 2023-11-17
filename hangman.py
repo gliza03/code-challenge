b1c0045863cbb6d22802458bbe9b546294c0c2f4
gliza03 <99229900+gliza03@users.noreply.github.com>
2023-11-16 20:35:52 -0700

    Test 2 (length of word)

--- hangman.py
+++ hangman.py
@@ -2,11 +2,13 @@ import random
 
 # Greeting
 name = input("What is your name?")
-print ("Hello &s, Welcome to Hangman!", name)
+print ("Hello {}, Welcome to Hangman!".format(name))
 
 # Define list of words
 words = ["charm", "pretty", "obstacle", "difficulty", "speaker", "Peru", "sun", "vitamins", "fan", "glasses"]
 
+# Randomize word and output length
 word = random.choice(words)
-print(word)
+print (word)
+print (len(word))
 
