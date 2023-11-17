884024c67506537dec0cd781925437a74460e087
gliza03 <99229900+gliza03@users.noreply.github.com>
2023-11-16 20:31:42 -0700

    Hangman

---
+++ hangman.py
@@ -0,0 +1,12 @@
+import random
+
+# Greeting
+name = input("What is your name?")
+print ("Hello %s, Welcome to Hangman!", name)
+
+# Define list of words
+words = ["charm", "pretty", "obstacle", "difficulty", "speaker", "Peru", "sun", "vitamins", "fan", "glasses"]
+
+word = random.choice(words)
+
+
--- testing.py
+++
@@ -1,9 +0,0 @@
-code = "name"
-count = 0
-for n in code:
-    count +=1
-    print(count)
-
-
-if n==4:
-    print("yay")
