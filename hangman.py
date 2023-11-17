11a8c70f7448b6f81d6fde456e30ce34415d593c
gliza03 <99229900+gliza03@users.noreply.github.com>
2023-11-16 21:35:32 -0700

    Final code, test 3

--- hangman.py
+++ hangman.py
@@ -25,6 +25,8 @@ def hangman():
 
         if type(letter) != str or len(letter)>1:
             print("Invalid!")
+            letter = input("Guess a letter:\n").lower()
+            
 
         if letter not in lettersUsed:
             lettersUsed.append(letter)
